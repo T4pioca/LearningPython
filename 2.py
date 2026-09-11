f68b88626103c81c52e391c34b70e986a858d268
Shameer <ma.shameer30@gmail.com>
2026-09-11 19:28:50 +0800

    Practicing Python from scratch

---
+++ 2.py
@@ -0,0 +1,22 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+# Integers
+age = 22
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
---
+++ 4.py
@@ -0,0 +1,31 @@
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+from operator import itemgetter
+
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
+#
+# age = age + 1
+#
+# print(f"Hello {name}!")
+# print("HAPPY BIRTHDAY!")
+# print(f"You are {age} years old.")
+
+
+# Exercise 1
+# length = float(input("Enter length: "))
+# width = float(input("Enter width: "))
+#
+# area = length * width
+#
+# print(f"The area of the rectangle is {area}cm²")
+
+# Exercise 2
+
+item = input("What item would you like to buy?: ")
+price = float(input(f"What is the price of the {item}?: "))
+quantity = int(input(f"How many {item} would you like?: "))
+
+total = price * quantity
+print(f"You have bought {quantity} x {item}/s.")
+print(f"Your total is: ${total}")
