ccd21ccead9d574c04bb2e49d3049253b666aa4f
Shameer <ma.shameer30@gmail.com>
2026-09-13 22:48:16 +0800

    Did some coding exercise

---
+++ 7.py
@@ -0,0 +1,35 @@
+# if = Do some code only If some condition is true
+#      Else do something
+
+# age = int(input("Enter your age?: "))
+#
+# if age >= 100:
+#     print("You are too old to signup!")
+# elif age > 18:
+#     print("You are now signed up!")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# else:
+#     print("You must be 18+ to sign up.")
+
+# response = input("Would you like food? (y/n): ")
+#
+# if response == "y":
+#     print("Have some food!")
+# elif response == "n":
+#     print("No food for you!")
+# else:
+#     print("Bye")
+
+# name = input("Enter your name: ")
+#
+# if name == "":
+#     print("You didn't enter your name!")
+# else:
+#     print(f"Hello {name}!")
+
+online = True
+if online:
+    print("The user is online!")
+else:
+    print("The user is offline!")
